95748815d06fb590607166aa8d63c836804e1f2b
Tamas <tamas.fehertoi@gmail.com>
2024-08-04 11:28:12 -0500

    home page finished

--- my-project/src/components/Hero/Hero.jsx
+++ my-project/src/components/Hero/Hero.jsx
@@ -156,21 +156,32 @@ const Hero = () => {
               <div className="grid grid-cols-3 gap-10">
                 {headphoneData.map((item) => {
                   return (
-                    <div
-                      key={item.id}
-                      onClick={() => handleActiveData(item)}
-                      className="grid grid-cols2 place-items-center cursor-pointer"
+                    <UpdateFollower
+                      mouseOptions={{
+                        backgroundColor: item.bgColor,
+                        zIndex: 9999,
+                        followSpeed: 0.5,
+                        scale: 5,
+                        text: "View Details",
+                        textFontSize: "3px",
+                      }}
                     >
-                      <div>
-                        <img src={item.image} alt="" className="w-[200px]" />
+                      <div
+                        key={item.id}
+                        onClick={() => handleActiveData(item)}
+                        className="grid grid-cols2 place-items-center cursor-pointer"
+                      >
+                        <div>
+                          <img src={item.image} alt="" className="w-[200px]" />
+                        </div>
+                        <div className="space-y-2">
+                          <p className="text-base font-bold">{item.price}</p>
+                          <p className="text-xs font-normal text-nonwrap">
+                            {item.modal}
+                          </p>
+                        </div>
                       </div>
-                      <div className="space-y-2">
-                        <p className="text-base font-bold">{item.price}</p>
-                        <p className="text-xs font-normal text-nonwrap">
-                          {item.modal}
-                        </p>
-                      </div>
-                    </div>
+                    </UpdateFollower>
                   );
                 })}
               </div>

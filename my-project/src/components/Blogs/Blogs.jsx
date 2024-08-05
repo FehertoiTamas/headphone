931d80a36837d1d85a3eb191f8f5767a75e03dde
Tamas <tamas.fehertoi@gmail.com>
2024-08-05 13:12:14 -0500

    Add Blogs component & create BlogsData

---
+++ my-project/src/components/Blogs/Blogs.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Blogs1 from "../../assets/blogs/blog1.jpg";
+import Blogs2 from "../../assets/blogs/blog2.jpg";
+import Blogs6 from "../../assets/blogs/blog3.jpg";
+import Blogs4 from "../../assets/blogs/blog14.jpg";
+import { motion } from "framer-motion";
+import { UpdateFollower } from "react-mouse-follower";
+
+const BlogsData = [
+  {
+    id: 1,
+    title: "Lorem ipsum dolor sit amet.",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis consequuntur accusamus perferendis cum ipsum, molestiae excepturi eius doloremque voluptates quas.",
+    link: "#",
+    img: "Blogs1",
+  },
+
+  {
+    id: 2,
+    title: "Lorem ipsum dolor sit amet.",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis consequuntur accusamus perferendis cum ipsum, molestiae excepturi eius doloremque voluptates quas.",
+    link: "#",
+    img: "Blogs2",
+  },
+
+  {
+    id: 3,
+    title: "Lorem ipsum dolor sit amet.",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis consequuntur accusamus perferendis cum ipsum, molestiae excepturi eius doloremque voluptates quas.",
+    link: "#",
+    img: "Blogs3",
+  },
+
+  {
+    id: 4,
+    title: "Lorem ipsum dolor sit amet.",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis consequuntur accusamus perferendis cum ipsum, molestiae excepturi eius doloremque voluptates quas.",
+    link: "#",
+    img: "Blogs4",
+  },
+];
+
+const Blogs = () => {
+  return <div></div>;
+};
+
+export default Blogs;
